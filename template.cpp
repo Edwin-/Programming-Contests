8ef98be1386ed3ed680534bf64337a6dc609a30a
Edwin <e19nuclear@gmail.com>
2017-06-18 23:56:16 -0400

    template c++

---
+++ template.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Date : 00/00/2017
+ * Autor : Edwin Payrumani Mamani
+ * Judge: 
+ * Problem:
+ *****************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ALL(x)      x.begin(),x.end()
+#define SORT(c)     sort(ALL(c))
+#define CL(a,b)     memset(a,b,sizeof(a))
+
+#define snuke(c,it) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();it++)
+#define FX(x) fixed << setprecision(x)
+
+typedef long long   LL;
+typedef unsigned long long uLL;
+
+#define EPS (1e-9)
+#define PI acos(-1)
+#define oo (1<<30)
+
+int main(){
+       
+        return 0;
+}
