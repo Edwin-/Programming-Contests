880538cdde89ec2379258ad071ac4800f8978603
Edwin <e19nuclear@gmail.com>
2017-06-23 22:33:30 -0400

    template c++

--- template.cpp
+++ template.cpp
@@ -13,17 +13,21 @@ using namespace std;
 #define SORT(c)     sort(ALL(c))
 #define CL(a,b)     memset(a,b,sizeof(a))
 
-#define snuke(c,it) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();it++)
+#define foreach(c,it) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();it++)
 #define FX(x) fixed << setprecision(x)
+#define INF (1<<30)
 
 typedef long long   LL;
 typedef unsigned long long uLL;
 
-#define EPS (1e-9)
-#define PI acos(-1)
-#define INF (1<<30)
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
 
 int main(){
-       
+       	ios_base::sync_with_stdio(0);
+
+
+
+
         return 0;
 }
